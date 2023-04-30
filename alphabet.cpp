079e586962bb3ffa935f8da7b0faaf131d2c7c6f
S-D-Keerthiga-Devi <132081378+S-D-Keerthiga-Devi@users.noreply.github.com>
2023-04-30 17:51:02 +0530

    Add files via upload

---
+++ alphabet.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char ch;
+    cout<<"enter a character:";
+    cin>>ch;
+    if((ch>='a' && ch<='z')|| (ch>='A' && ch<='Z'))
+    {
+        cout<<ch<<" is an alphabet";
+    } 
+    else{
+        cout<<ch<<" is not an alphabet";
+    }
+    return 0;
+}
